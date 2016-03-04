d821774125deaa2e089ec957fddd22a9892b3a3d
MitchellCox <mitchell.allan.cox@gmail.com>
2016-03-03 20:41:49 -0700

    Random Answers Appear

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-var question = function(question)
-var randomAnswer = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'Most likely', 'No, of course not', 'Sources say no', 'Not on my watch']
+// var Question = function(answers)
 
-question.forEach(function(question) {
-	randomAnswer;
-}); 
+function answer() {
+	var words = document.getElementById('words');
+	var randomAnswers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'Most likely', 'No, of course not', 'Sources say no', 'Not on my watch']
+	var randomAnswer = randomAnswers[Math.floor(Math.random() * randomAnswers.length)]
+	words.innerHTML = randomAnswer;
+	}
+
+
+// Question(randomAnswer) {
+
+// }
+
