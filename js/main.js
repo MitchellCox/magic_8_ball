d4e4cf99defc3dae525bebf1bcab0301d612bd53
MitchellCox <mitchell.allan.cox@gmail.com>
2016-03-03 19:55:41 -0700

    8 ball v1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var question = function(question)
+var randomAnswer = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'Most likely', 'No, of course not', 'Sources say no', 'Not on my watch']
+
+question.forEach(function(question) {
+	randomAnswer;
+}); 
